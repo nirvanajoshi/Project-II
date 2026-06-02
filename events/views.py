0441abedcaf6bf742fef62a3ce56bab4afca593d
nirvanajoshi <damodarj36@gmail.com>
2026-06-02 06:52:28 +0545

    Initial commit

---
+++ events/views.py
@@ -0,0 +1,24 @@
+
+
+# Create your views here.
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
+def home(request):
+    return render(request, 'home.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)   # log the new user in automatically
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
